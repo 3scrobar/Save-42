311b2aae0af0ff48a95c255db07b1db4d15b598c
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 11:47:54 +0100

    autosave

--- ftlib/ft_strlcat.c
+++ ftlib/ft_strlcat.c
@@ -5,38 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 15:25:29 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 11:45:41 by tle-saut         ###   ########.fr       */
+/*   Created: 2024/11/06 11:47:40 by tle-saut          #+#    #+#             */
+/*   Updated: 2024/11/06 11:47:50 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	dst_len;
+	size_t	src_len;
 	size_t	i;
-	size_t	c;
-	size_t	initsrc;
 
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+
+	// Si `size` est inférieur ou égal à la longueur de `dst`, on retourne `size + src_len`
+	if (size <= dst_len)
+		return (size + src_len);
+
+	// On copie les caractères de `src` dans `dst`, sans dépasser `size - 1` pour garder la place du `\0`
 	i = 0;
-	c = ft_strlen(dst);
-	initsrc = ft_strlen(src);
-	while (i <= size - 1 && src[i])
+	while (src[i] && (dst_len + i) < (size - 1))
 	{
-		dst[c + i] = src[i];
+		dst[dst_len + i] = src[i];
 		i++;
 	}
-	if (initsrc < size)
-		dst[c + i] = 0;
-	return (initsrc + c);
-}
-int	main(void)
-{
-	char *dest;
-	
-	dest[10] = 'a';
-    ft_print_result(ft_strlcat(dest, "lorem ipsum dolor sit amet", 1));
-    write(1, "\n", 1);
-    write(1, dest, 15);
+	// On ajoute le caractère nul `\0`
+	dst[dst_len + i] = '\0';
 
+	// Retourne la longueur totale de la chaîne qu'on a tenté de créer
+	return (dst_len + src_len);
 }
