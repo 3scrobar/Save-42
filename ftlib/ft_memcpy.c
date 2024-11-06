8afc6096e4e48d9b249d76256b9e7d93f57c2b98
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 14:07:14 +0100

    autosave

--- ftlib/ft_memcpy.c
+++ ftlib/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:34:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 13:56:28 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:06:01 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	c;
 
 	c = 0;
-	while (c != n)
+	while (c < n)
 	{
 		*(unsigned char *)(dest + c) = *(unsigned char *)(src + c);
 		c++;
 	}
 	return (dest);
 }
+
