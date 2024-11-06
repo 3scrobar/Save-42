c184568759a6335700b9e01e0a53792ce2786b1e
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 15:51:25 +0100

    autosave

--- ftlib/ft_strncmp.c
+++ ftlib/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:54:57 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 15:47:42 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:51:24 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, int n)
 	i = 0;
 	if (n <= 0)
 		return (0);
-	while (s1[i] && s1[i] == s2[i] && i < n - 1)
+	while ((unsigned char *)[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
