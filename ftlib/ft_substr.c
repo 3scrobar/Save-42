4abe36a9fbec3f7243ca31bec9143fe019e9bbb5
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 16:49:42 +0100

    autosave

--- ftlib/ft_substr.c
+++ ftlib/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:31:56 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 16:47:37 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:49:40 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 char *ft_substr(char const *s, unsigned int start,
 size_t len)
 {
-	int	i;
+	int				i;
 	unsigned char *	str;
-	i = start;
 	
+	i = start;
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
 	while (s[i])
 	{
-		
+		str[i - start]
 	}
 	
 }
