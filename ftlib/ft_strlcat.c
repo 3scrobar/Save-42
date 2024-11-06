79547294e6db0fc1d1cb6dea168805ddbe93b470
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 11:07:38 +0100

    autosave

--- ftlib/ft_strlcat.c
+++ ftlib/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:25:29 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 11:01:54 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 11:07:20 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	i = 0;
 	c = ft_strlen(dst);
 	initdest = ft_strlen(dst);
-	while (i != size - 1 && src[i])
+	while (i < size - 1 && src[i])
 	{
 		dst[c + i] = src[i];
 		i++;
 	}
 	dst[c + i] = 0;
-	return (ft_strlen(dst) + ft_strlen(src));
+	return (initdest + ft_strlen(src));
 }
