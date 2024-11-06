2c04bd49f99d781191cca7a078467e2ce1b1770c
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 13:52:48 +0100

    autosave

--- ftlib/ft_calloc.c
+++ ftlib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:23:28 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/05 17:24:56 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 13:52:12 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	int		i;
 	size_t	total_size;
 
+	if (size <= 0)
+		return (NULL);
 	total_size = nmemb * size;
 	i = 0;
 	if (size <= 0)
