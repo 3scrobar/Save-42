9688240691d1c34a48c8331bbd830cfd3ff505dc
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 14:29:58 +0100

    autosave

--- ftlib/ft_calloc.c
+++ ftlib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:23:28 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 14:21:40 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:29:47 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 void *ft_calloc(size_t nmemb, size_t size)
 {
-    void *ptr;
-    size_t total_size;
+	void *ptr;
+	size_t total_size;
 
-
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    total_size = nmemb * size;
-    ptr = malloc(total_size);
-    if (ptr == NULL)
-        return (NULL);
-    ft_memset(ptr, 0, total_size);
-    return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memset(ptr, 0, total_size);
+	return (ptr);
 }
 
--- ftlib/ft_memmove.c
+++ ftlib/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:40:37 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 14:26:21 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:28:07 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	c;
 
-	if (dest == NULL || src == NULL)
-		return(NULL);
 	c = 0;
 	if (dest < src)
 	{
