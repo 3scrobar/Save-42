5f17b1ffc0033657fac6dbc07819b76ce5aaf95d
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 13:48:40 +0100

    autosave

--- ftlib/ft_strdup.c
+++ ftlib/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:46:53 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/05 17:39:54 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 13:48:37 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ char	*ft_strdup(const char *s)
 	size_t	i;
 
 	i = 0;
-	dest = malloc(ft_strlen(s) * sizeof(char));
+	dest = malloc((ft_strlen(s) + 1) * sizeof(char));  // +1 pour le caractère nul
 	if (dest == NULL)
 		return (NULL);
+
 	while (s[i])
 	{
 		dest[i] = s[i];
 		i++;
 	}
+
+	dest[i] = '\0';
 	return (dest);
 }
+
