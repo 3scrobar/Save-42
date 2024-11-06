3363dd11e5f29f74bdbf0df95cc053bc2228fd0b
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 17:00:02 +0100

    autosave

--- ftlib/ft_strjoin.c
+++ ftlib/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:52:17 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 16:57:52 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:59:59 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-	size_t			s1len;
-	size_t			s2len;
+
 	size_t			i;
+	size_t			j;
 	unsigned char *	dest;
 
 	i = 0;
-	s2len = ft_strlen(s2);
-	s1len = ft_strlen(s1);
-	dest = malloc(sizeof(char) * (s2len + s1len));
+	j = 0;
+
+	dest = malloc(sizeof(char) * (ft_strlen(s2) + ft_strlen(s1));
 	if (!dest)
 		return (NULL);
 	while (!s1[i])
 	{
-		dest[i] =
+		dest[i] = s1[i];
+		i++;
+	}
+	while (!s2[i])
+	{
+		dest[i + j] = s2[j];
+		j++;
 	}
 	
 }
