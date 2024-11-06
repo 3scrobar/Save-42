c523e5bb4bbd129c617658d6f619787267c83bf0
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 14:01:02 +0100

    autosave

--- ftlib/ft_strchr.c
+++ ftlib/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:31:43 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 13:58:36 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:01:01 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-	if (ft_isprint(c) == 1)
-		i = 0;
-		while (s[i] && s[i] != c)
-			i++;
-		if (s[i] != c)
-			return (NULL);
+	int i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (s[i] == (char)c)
 		return ((char *)s + i);
+	return (NULL);
 }
