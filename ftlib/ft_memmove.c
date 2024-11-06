b3a0adfc1543d466d86a6b17ee016626ee6b9784
Thomas Le Sauter <tle-saut@c2r4p4.42perpignan.fr>
2024-11-06 14:09:18 +0100

    autosave

--- ftlib/ft_memmove.c
+++ ftlib/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:40:37 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/06 14:04:54 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:08:58 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	c;
 
+	if (dest == NULL || src == NULL || n == NULL)
+		return (NULL);
 	c = 0;
 	if (dest < src)
 	{
