eaa4e9519b63e4beee17488efc5c8b416b2ba6b0
Thomas Le Sauter <tle-saut@c2r8p8.42perpignan.fr>
2024-11-08 13:43:11 +0100

    autosave

--- ftlib/ft_strtrim.c
+++ ftlib/ft_strtrim.c
@@ -6,78 +6,23 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:18:59 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/08 13:25:15 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:43:05 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_movestr(char *str, size_t start, size_t stop)
-{
-	size_t	i;
-
-	i = 0;
-	while (i <= stop)
-	{
-		str[i] = str[i + start];
-		i++;
-	}
-	str[i] = 0;
-	return (str);
-}
-
-static char	*ft_malloc_trim(char *s1, char const *set)
-{
-	size_t	cpt;
-	size_t	i;
-	char	*res;
-	size_t	check;
-	char	c;
-
-	i = 0;
-	cpt = 0;
-	check = 0;
-	while (s1[i])
-	{
-		c = s1[i];
-		check = ft_strchr(c, (char *)set);
-		if (check == 1)
-			cpt++;
-		check = 0;
-		i++;
-	}
-	
-	res = malloc(sizeof(char) * (ft_strlen(s1) - cpt));
-	if (res == NULL)
-		return (NULL);
-	return (res);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*res;
 	size_t	i;
-	size_t	yes;
-	size_t	loc;
-	size_t	stop;
 
-	stop = 0;
-	loc = 0;
-	yes = 0;
-	i = 0;
-	res = (char *)s1;
-	res = ft_malloc_trim(res, (char *)set);
-	if (res == NULL)
-		return (NULL);
-	while (s1[i])
+	while (s1 && ft_strchr((char *)set, s1) != NULL)
+		s1++;
+	i = ft_strlen((char *)s1) - 1;
+	while (ft_strrchr() != NULL)
 	{
-		yes = ft_searchchar(s1[i], (char *)set);
-		stop = ft_searchchar(s1[i + 1], (char *)set);
-		if (yes == 1 && stop == 0)
-			loc = i + 1;
-		if (stop == 0 && yes == 1)
-			stop = i + 1;
-		i++;
+		
 	}
-	return (ft_movestr((char *)s1, loc, stop));
+	
+	
 }
