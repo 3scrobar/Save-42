524ec32a509194e7c94c7cf94f1d2a32d1d352a3
Thomas Le Sauter <tle-saut@c2r8p8.42perpignan.fr>
2024-11-08 13:48:15 +0100

    autosave

--- ftlib/ft_strtrim.c
+++ ftlib/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:18:59 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/08 13:43:05 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:47:01 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1 && ft_strchr((char *)set, s1) != NULL)
 		s1++;
 	i = ft_strlen((char *)s1) - 1;
-	while (ft_strrchr() != NULL)
-	{
-		
-	}
-	
-	
+	while (ft_strrchr((char *)set, s1) != NULL)
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
