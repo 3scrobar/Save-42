72106a2f63e8d99ba83475bb69f33434878132b2
Thomas Le Sauter <tle-saut@c2r8p8.42perpignan.fr>
2024-11-08 17:00:30 +0100

    autosave

--- ftlib/ft_split.c
+++ ftlib/ft_split.c
@@ -6,14 +6,14 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:46:21 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/08 16:55:21 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/08 16:59:10 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //permet de separer une chaine en tableaux
 //en utilisant c, renvoi un tableau de chaine
-static int	ft_countword(char * s, char c)
+static int	ft_countword(char *s, char c)
 {
 	int	i;
 	int	cpt;
@@ -28,7 +28,7 @@ static int	ft_countword(char * s, char c)
 			i++;
 		i++;
 	}
-	return (i);
+	return (i + 1);
 }
 
 char	**ft_split(char const *s, char c)
@@ -38,12 +38,17 @@ char	**ft_split(char const *s, char c)
 	size_t	index;
 	int		word;
 
-	tab = malloc(ft_countword((char *)s,c) + 1 * (sizeof(char *)));
+	word = ft_countword((char *)s, c);
+	tab = malloc(word + 1 * (sizeof(char *)));
+	if (tab == NULL)
+		return (NULL);
 	i = 0;
 	while (tab[i])
 	{
 		index = ft_strindex((char *)s, c);
 		tab[i] = ft_substr(s, index, (unsigned long)ft_strchr(s + 1, c));
+		if (tab[i] == NULL)
+			return (tab);
 		s = s + index;
 		i++;
 	}
