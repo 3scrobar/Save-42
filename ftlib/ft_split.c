eeff222db910e886d88223bac12a06c9b50857ae
Thomas Le Sauter <tle-saut@c2r8p8.42perpignan.fr>
2024-11-08 17:35:55 +0100

    autosave

--- ftlib/ft_split.c
+++ ftlib/ft_split.c
@@ -6,13 +6,28 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 14:46:21 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/08 17:28:11 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/08 17:33:59 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 //permet de separer une chaine en tableaux
 //en utilisant c, renvoi un tableau de chaine
+static int	ft_strindex(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (0);
+}
 static int	ft_countword(char *s, char c)
 {
 	int	i;
@@ -39,7 +54,7 @@ char	**ft_split(char const *s, char c)
 	int		word;
 
 	word = ft_countword((char *)s, c);
-	tab = (char **)malloc((word + 1 )* (sizeof(char)));
+	tab = (char **)malloc((word + 1 )* (sizeof(char *)));
 	if (tab == NULL)
 		return (NULL);
 	i = 0;
