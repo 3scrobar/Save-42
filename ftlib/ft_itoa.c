2fbb23fa0e027c36fb3b0f7bd640eac60aad0820
Thomas Le Sauter <tle-saut@c1r3p2.42perpignan.fr>
2024-11-09 16:47:02 +0100

    AutoSave 2024-11-09 16:47:02

--- ftlib/ft_itoa.c
+++ ftlib/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 14:38:30 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/09 16:42:00 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/09 16:47:00 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	ft_nlen(int n)
 		len++;
 	if (n < 0)
 		n = -n;
-	while (n)
+	while (n > 0)
 	{
 		n /= 10;
 		len++;
@@ -57,11 +57,10 @@ char	*ft_itoa(int n)
 	i = 0;
 	negative = 0;
 	str = malloc(sizeof(char) * (ft_nlen(n) + 1));
-	if ()
-	if (n == 0)
-		str = "0";
+	if (!str)
+		return (NULL);
 	else if (n == -2147483648)
-		str = "-2147483648";
+		str = ft_strdup("-2147483648");
 	else if (n < 0)
 		negative = 1;
 	while (n != 0 && str != "-2147483648")
@@ -71,7 +70,7 @@ char	*ft_itoa(int n)
 	}
 		str[i + 1] = 0;
 	str = ft_reverse(str, i, negative);
-	return (ft_strdup(str));
+	return (str);
 }
 
 #include <stdio.h>
