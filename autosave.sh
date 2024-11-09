910a6ae0fe602ce833ff5d167bc3d29a204381ca
Thomas Le Sauter <tle-saut@c1r3p2.42perpignan.fr>
2024-11-09 15:05:29 +0100

    AutoSave 2024-11-09 15:05:29

--- autosave.sh
+++ autosave.sh
@@ -9,7 +9,7 @@ while true; do
 	sleep 1  # Attendre 1 seconde (facultatif)
 	# Utilisation de 'date' pour formater l'heure et la date
 	# $(date "+%Y-%m-%d %H:%M:%S") : format personnalisé pour inclure année, mois, jour, heure, minute et seconde
-	git commit -m "autosave $(date "+%Y-%m-%d %H:%M:%S")"  # Commit avec la date et l'heure dans le message
+	git commit -m "AutoSave $(date "+%Y-%m-%d %H:%M:%S")"  # Commit avec la date et l'heure dans le message
 	sleep 1  # Attendre 1 seconde avant de pousser (facultatif)
 	echo "Le commit a ete fait"
 	git push  # Pousser les changements vers le dépôt distant
