e62b2fad0ce6032a6823050b9f1d885a0c3b7e44
Thomas Le Sauter <tle-saut@c1r3p2.42perpignan.fr>
2024-11-09 15:06:31 +0100

    AutoSave 2024-11-09 15:06:31

--- autosave.sh
+++ autosave.sh
@@ -6,15 +6,13 @@ while true; do
 	cd /home/tle-saut/Gitperso
 	git add .  # Ajoute tous les fichiers modifiés au staging area
 	echo "Tout a ete add "
-	sleep 1  # Attendre 1 seconde (facultatif)
 	# Utilisation de 'date' pour formater l'heure et la date
 	# $(date "+%Y-%m-%d %H:%M:%S") : format personnalisé pour inclure année, mois, jour, heure, minute et seconde
-	git commit -m "AutoSave $(date "+%Y-%m-%d %H:%M:%S")"  # Commit avec la date et l'heure dans le message
-	sleep 1  # Attendre 1 seconde avant de pousser (facultatif)
+	git commit -m "AutoSave $(date "+%Y-%m-%d %H:%M:%S")"
 	echo "Le commit a ete fait"
 	git push  # Pousser les changements vers le dépôt distant
 	clear
-	echo "tout a ete mise a jour"
+	echo "Tout a ete mise a jour"
 	echo " 5 minute avant la prochaine auto save............"
 	sleep 60
 	clear
