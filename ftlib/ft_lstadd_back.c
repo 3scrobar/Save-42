eb16878280b0fa1fc899c5dff39acf69790b5e4e
Thomas Le Sauter <tle-saut@c2r6p1.42perpignan.fr>
2024-11-12 16:41:25 +0100

    AutoSave 2024-11-12 16:41:25

--- ftlib/ft_lstadd_back.c
+++ ftlib/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:35:39 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/12 16:36:00 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:41:23 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	new;
+
+	if (lst == NULL)
+		*
+	new = lst;
 	
+	while (lst -> next != NULL)
+		lst = lst -> next;
 }
