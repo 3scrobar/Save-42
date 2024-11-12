93cb1a4015ffe7a88507e0299387649e65a3c001
Thomas Le Sauter <tle-saut@c2r6p1.42perpignan.fr>
2024-11-12 16:26:18 +0100

    AutoSave 2024-11-12 16:26:18

--- ftlib/libft.h
+++ ftlib/libft.h
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 11:50:09 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/12 16:20:39 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:25:17 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,8 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+
 
 
 
