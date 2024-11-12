213588e34cf87b59b88b1cee7d5c712b20904fdd
kali_maison <corsica-man@hotmail.fr>
2024-11-12 19:07:40 +0100

    AutoSave 2024-11-12 19:07:40

--- ftlib/ft_lstmap.c
+++ ftlib/ft_lstmap.c
@@ -6,11 +6,22 @@
 /*   By: tle_saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:01:34 by tle_saut          #+#    #+#             */
-/*   Updated: 2024/11/12 19:02:03 by tle_saut         ###   ########.fr       */
+/*   Updated: 2024/11/12 19:07:04 by tle_saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),
 void (*del)(void *))
 {
+	t_list	new_one;
+
+	new_one = malloc(sizeof(t_list));
+
+	while (lst != NULL)
+	{
+		
+	}
+	
 }
