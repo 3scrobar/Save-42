c0f51340c741ffa74f0edcba16fcfe1f44895c27
kali_maison <corsica-man@hotmail.fr>
2024-11-13 12:51:49 +0100

    AutoSave 2024-11-13 12:51:49

--- ftlib/ft_lstadd_back.c
+++ ftlib/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: tle_saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 16:35:39 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/13 12:19:35 by tle_saut         ###   ########.fr       */
+/*   Updated: 2024/11/13 12:49:19 by tle_saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*new_one;
 
-	if (*lst == NULL)  // Si la liste est vide
+	if (*lst == NULL)
 	{
-		*lst = new;   // Le nouvel élément devient le premier
+		*lst = new;
 		return ;
 	}
 	new_one = *lst;
-	while (new_one->next != NULL)  // Trouver le dernier élément
+	while (new_one->next != NULL)
 		new_one = new_one->next;
-	new_one->next = new;  // Ajouter le nouvel élément à la fin
+	new_one->next = new;
 }
-
