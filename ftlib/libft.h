b7cce0ec35d5f34d50503e4bbef2768364ed0a0a
kali_maison <corsica-man@hotmail.fr>
2024-11-13 13:26:13 +0100

    AutoSave 2024-11-13 13:26:13

--- ftlib/libft.h
+++ ftlib/libft.h
@@ -6,7 +6,7 @@
 /*   By: tle_saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 11:50:09 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/13 12:14:59 by tle_saut         ###   ########.fr       */
+/*   Updated: 2024/11/13 13:22:15 by tle_saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
