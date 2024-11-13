ad513c17b62929a6f6aaf3000aea5d3a37538809
kali_maison <corsica-man@hotmail.fr>
2024-11-13 13:19:57 +0100

    AutoSave 2024-11-13 13:19:57

--- ftlib/ft_lstmap.c
+++ ftlib/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tle_saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:01:34 by tle_saut          #+#    #+#             */
-/*   Updated: 2024/11/13 12:48:27 by tle_saut         ###   ########.fr       */
+/*   Updated: 2024/11/13 13:19:54 by tle_saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*current;
 	t_list	*result;
 	t_list	*new_node;
 
 	result = NULL;
-	while (lst != NULL)
+	current = lst;
+	if (!lst || !f)
+		return (NULL);
+	while (current != NULL)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_node = ft_lstnew(f(current->content));
 		if (new_node == NULL)
 		{
 			ft_lstclear(&result, del);
-			ft_lstdelone(new_node, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&result, new_node);
-		lst = lst->next;
+		current = current->next;
 	}
 	return (result);
 }
