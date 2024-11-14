0d2afb28394b231dc4c2d443f86f31ee032f845e
Thomas Le Sauter <tle-saut@c2r6p8.42perpignan.fr>
2024-11-14 15:44:29 +0100

    AutoSave 2024-11-14 15:44:29

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/14 15:39:17 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:44:23 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 int ft_printf(const char *str, ...)
 {
-	char	selec;
-	int		i;
+	char			selec;
+	int				i;
+	unsigned int	cpt;
 	va_list	args;
 	va_start(args, str);
 
 	i = 0;
+	cpt = 0;
 	while (str[i])
 	{
 		if (str[i]!= '%')
 			write(1, &str[i], 1);
 		else
-			{
-				i++;
-				selec
-			}
+			selec = ft_check(str[++i]);
+		if (selec == 'c')
+			cpt = cpt + ft_printchar(va_arg(args, char));
+		if (selec == 's')
+			cpt = cpt + ft_printstr(va_arg(args, char *));
 	}
 }
