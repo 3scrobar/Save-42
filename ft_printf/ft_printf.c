783f4d4d5f0898e53f21407885ef52ff4c5cd08e
Thomas Le Sauter <tle-saut@c2r6p8.42perpignan.fr>
2024-11-14 13:54:23 +0100

    AutoSave 2024-11-14 13:54:23

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,10 +6,21 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/14 13:46:01 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:54:19 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *str, ...)
+{
+	int		i;
+	va_list	args;
+	va_start(args, str);
+	
+	
+	while (str)
+	{
+		
+	}
+}
--- ft_printf/printf.h
+++ ft_printf/printf.h
@@ -6,11 +6,13 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:38:24 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/14 13:45:11 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:52:24 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_H
 # define PRINTF_H
+
+#include <stdarg.h>
 
 #endif
