3380415d6cc629b0e5d278446e1a60c91df7d104
Thomas Le Sauter <tle-saut@c2r6p8.42perpignan.fr>
2024-11-14 15:54:32 +0100

    AutoSave 2024-11-14 15:54:32

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/14 15:49:30 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:54:28 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,15 @@ int ft_printf(const char *str, ...)
 			cpt = cpt + ft_printstr(va_arg(args, char *));
 	}
 }
+#include <stdio.h>
 
 int main(void)
 {
-	char c =
+	char c = 'c';
+	char *str = "test";
+	int cpt = 0;
+	
+	cpt = ft_printf("je te le char %c, et je teste le string %s",c,str);
+	printf("\njai ecrit %i", cpt);
+	return (0);
 }
