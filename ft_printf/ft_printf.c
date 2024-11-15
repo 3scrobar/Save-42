aaefa9eeb0363b3ff61af671afa2757418688abf
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 16:49:08 +0100

    AutoSave 2024-11-15 16:49:08

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 16:43:47 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:44:37 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,3 @@ unsigned int ft_printf(const char *str, ...)
 	return (cpt);
 }
 
-#include <stdio.h>
-
-int main(void)
-{
-
-	int cpt = 0;
-
-	cpt = ft_printf(" %p\n", 17);
-	printf("jai ecrit %i\n", cpt);
-	printf("printf %p\n", 17);
-	printf("jai ecrit %i\n", cpt);
-
-	return (0);
-}
