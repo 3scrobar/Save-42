25f9a3d6d2c16700d3ca83d6620a9d653775304b
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 17:19:17 +0100

    AutoSave 2024-11-15 17:19:17

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 17:14:14 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 17:15:09 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ unsigned int ft_printf(const char *str, ...)
 }
 
 #include <stdio.h>
-#include <limits>
+#include <limits.h>
 
 int main(void)
 {
