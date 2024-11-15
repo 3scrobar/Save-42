292917926d5eb2fe9e672f71cbe725b01f704301
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 15:26:18 +0100

    AutoSave 2024-11-15 15:26:18

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 15:20:11 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 15:25:57 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ unsigned int ft_printf(const char *str, ...)
 	va_start(args, str);
 	
 	cpt = 0;
-	while (str)
+	while (*str)
 		{
 			if (*str != '%')
+			{
+				cpt += write(1, str, 1);
 				str++;
+			}
 			else
 			{
 				str++;
