aade1b45caa0239317f052891dcdd6c8c6695e55
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 15:01:10 +0100

    AutoSave 2024-11-15 15:01:10

--- ft_printf/ft_printfnumb_base_min.c
+++ ft_printf/ft_printfnumb_base_min.c
@@ -6,8 +6,13 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:31:57 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 14:32:26 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 15:00:52 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printfnumb_base_min()
+int	ft_printfnumb_base_min(unsigned int nbr)
+{
+	unsigned int	cpt;
+
+	
+}
