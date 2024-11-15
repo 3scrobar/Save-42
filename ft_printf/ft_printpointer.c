6348afe8b5a76b671c3ac99d0cb48c950b2d4944
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 16:08:56 +0100

    AutoSave 2024-11-15 16:08:56

--- ft_printf/ft_printpointer.c
+++ ft_printf/ft_printpointer.c
@@ -6,10 +6,13 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:01:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 16:03:53 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:03:56 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	ft_printpointer(size_t point)
+{
+	
+}
