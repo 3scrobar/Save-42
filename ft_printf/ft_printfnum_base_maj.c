c698f76abc7ea00bd54929f3f56f234995d96150
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 15:06:12 +0100

    AutoSave 2024-11-15 15:06:12

--- ft_printf/ft_printfnum_base_maj.c
+++ ft_printf/ft_printfnum_base_maj.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printfnumb_base_min.c                           :+:      :+:    :+:   */
+/*   ft_printfnum_base_maj.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 14:31:57 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 15:05:39 by tle-saut         ###   ########.fr       */
+/*   Created: 2024/11/15 15:06:05 by tle-saut          #+#    #+#             */
+/*   Updated: 2024/11/15 15:06:10 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printfnumb_base_min(unsigned int nbr)
-{
-	unsigned int	cpt;
-	char 			*str;
-	
-	cpt = 0;
-	str = "0123456789abcdef";
-	if (nbr > 16)
-		ft_printfnumb_base_min(nbr / 16);
-	cpt += write(1, &str[nbr % 16], 1);
-	return (nbr);
-}
+#include ""
