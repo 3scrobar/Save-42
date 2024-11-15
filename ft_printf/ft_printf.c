b7ac578fee779be335f672d4db8228b5443fa731
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-11-15 15:43:48 +0100

    AutoSave 2024-11-15 15:43:48

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:37:33 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/15 15:38:05 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/15 15:43:40 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,10 @@ unsigned int ft_printf(const char *str, ...)
 
 int main(void)
 {
-	char c = 'c';
-	char *str = "test";
 	int cpt = 0;
 	
-	cpt = ft_printf("je test le char \n%c\nje teste le string \n%s\nje teste les nombre\n%d\n%i\n les bases\n%x\n%X\n et pourcent \n%%\n",c,str,42,21,17,17);
+	cpt = ft_printf("%s %p %z", (char *)NULL, NULL);
+	printf("%s %p %z", (char *)NULL, NULL);
 	printf("jai ecrit %i", cpt);
 	return (0);
 }
