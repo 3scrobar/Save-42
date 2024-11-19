c488446a5f86cdf1997accb6d244dcb69a60a81b
Thomas Le Sauter <tle-saut@c2r3p4.42perpignan.fr>
2024-11-19 14:43:50 +0100

    AutoSave 2024-11-19 14:43:50

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/18 15:53:24 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:43:24 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,35 @@
 
 char	*get_next_line(int fd)
 {
+	static char *toprint;
 	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	toprint = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (toprint == NULL)
+		return (NULL);
+	if (read(fd, toprint, BUFFER_SIZE) > 0);
+		return (toprint);
+	else
+	{
+		free(toprint);
+		return (NULL);
+	}
+}
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("essai.txt", O_RDONLY);
+	while (get_next_line(fd) != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s\n", line);
+	}
+	
+
+	free(line);
+	close(fd);
+	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,23 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:38 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/18 15:54:46 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:35:41 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 char	*get_next_line(int fd);
 
-
 #endif
+
