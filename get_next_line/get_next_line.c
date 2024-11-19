4717547822a621b7490a755f9bd5277acfe41172
Thomas Le Sauter <tle-saut@c2r3p4.42perpignan.fr>
2024-11-19 15:06:33 +0100

    AutoSave 2024-11-19 15:06:33

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/19 14:59:27 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:05:01 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,26 @@ char	*get_next_line(int fd)
 	return (toprint);
 }
 int main(void)
+{
+	int fd;
+	char *line;
 
+	// Ouvrir le fichier en lecture
+	fd = open("essai.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Erreur lors de l'ouverture du fichier");
+		return (1);
+	}
+
+	// Lire et afficher chaque ligne
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", line);
+		free(line); // N'oubliez pas de libérer la mémoire allouée par get_next_line
+	}
+
+	// Fermer le fichier
+	close(fd);
+	return (0);
+}
