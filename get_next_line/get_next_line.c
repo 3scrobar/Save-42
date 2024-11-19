9797257ce941ee80dcaa6cf3c93bd12773984803
Thomas Le Sauter <tle-saut@c2r3p4.42perpignan.fr>
2024-11-19 15:16:36 +0100

    AutoSave 2024-11-19 15:16:36

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/19 15:11:30 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:14:13 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ char	*get_next_line(int fd)
 	static char *temp;
 	static char *toprint;
 	int 		i;
-	
+	//check if somethings is wrong
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	//malloc the good size
 	toprint = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	//check some mistake
 	if (toprint == NULL)
 		return (NULL);
+	//read and store
 	i = read(fd, temp, BUFFER_SIZE);
-	toprint += temp;
 	if (i != NULL)
-		return (toprint);
+		{
+			
+		}
 }
 int main(void)
 {
