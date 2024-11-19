c4de5fa50a19075125354ecb1d411c7045f15679
Thomas Le Sauter <tle-saut@c2r3p4.42perpignan.fr>
2024-11-19 15:21:38 +0100

    AutoSave 2024-11-19 15:21:38

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,41 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/19 15:14:13 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:21:36 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && (dst_len + i) < (size - 1))
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = 0;
+	return (dst_len + src_len);
+}
 
 char	*get_next_line(int fd)
 {
@@ -26,11 +56,12 @@ char	*get_next_line(int fd)
 	if (toprint == NULL)
 		return (NULL);
 	//read and store
-	i = read(fd, temp, BUFFER_SIZE);
-	if (i != NULL)
+	while ((i = read(fd, temp, BUFFER_SIZE)) > 0)
 		{
 			
 		}
+	
+
 }
 int main(void)
 {
