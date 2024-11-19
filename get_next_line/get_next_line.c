33c21bee494914999ef3befb3cbd08d7f70f56d1
Thomas Le Sauter <tle-saut@c2r3p4.42perpignan.fr>
2024-11-19 14:56:30 +0100

    AutoSave 2024-11-19 14:56:30

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/19 14:43:24 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:56:26 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,8 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (read(fd, toprint, BUFFER_SIZE) > 0);
 		return (toprint);
-	else
-	{
-		free(toprint);
-		return (NULL);
-	}
+	free(toprint);
+	return (NULL);
 }
 int main(void)
 {
@@ -35,13 +32,8 @@ int main(void)
 	char *line;
 
 	fd = open("essai.txt", O_RDONLY);
-	while (get_next_line(fd) != NULL)
-	{
-		line = get_next_line(fd);
-		printf("%s\n", line);
-	}
-	
-
+	line = get_next_line(fd);
+	printf("%s\n", line);
 	free(line);
 	close(fd);
 	return (0);
