b792f40bac5e141f91e8bcffc0aa6a7f4f1d20a6
VM maison <corsica-man@hotmail.fr>
2024-11-20 15:47:23 +0100

    AutoSave 2024-11-20 15:47:23

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toto <toto@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:35 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/19 17:07:29 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/20 15:44:19 by toto             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,31 @@
 
 char	*get_next_line(int fd)
 {
-	
+	char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t			i;
+	size_t			j;
+	unsigned char	*dest;
+
+	i = 0;
+	j = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(s2) + ft_strlen(s1) + 1));
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = 0;
+	free(s1);
+	return ((char *)dest);
+}
 }
 int main(void)
 {
