97f35d31bcebc77999e10aa8a19a19105e3f0f36
Thomas Le Sauter <tle-saut@c2r3p6.42perpignan.fr>
2024-11-21 13:40:49 +0100

    AutoSave 2024-11-21 13:40:49

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:52:38 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/11/21 13:17:34 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:38:27 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 100
 # endif
 
 # include <unistd.h>
