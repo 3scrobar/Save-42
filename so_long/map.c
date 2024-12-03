8079411427b2d7df5054b3efdae6f6705c413f79
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-12-03 15:00:42 +0100

    AutoSave 2024-12-03 15:00:42

--- so_long/map.c
+++ so_long/map.c
@@ -6,9 +6,33 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:44 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/03 14:54:44 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/03 15:00:37 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_parsing(char **map, int *line, int *player, int *exit, int *collectible)
+{
+	int	cursor;
+
+	cursor = 0;
+		while (*map[cursor])
+	{
+		if (*map[0] != '0')
+			return (0);
+		else if (*map[cursor] == '\n')
+			*line += 1;
+		else if (*map[cursor] != "1" && *line ==0)
+			return (0);
+		else if (*map[cursor] == "P")
+			*player += 1;
+		else if (*map[cursor] == "E")
+			*exit += 1;
+		else if (*map[cursor] == "C")
+			*collectible += 1;
+		cursor++;
+	}
+	return (1);
+}
 
 int	ft_checkmap(char *map)
 {
@@ -20,22 +44,7 @@ int	ft_checkmap(char *map)
 
 	collectible = 0;
 	cursor = 0;
-	while (map[cursor])
-	{
-		if (map[0] != '0')
-			return (write(1, "Error, Map is invalide",22),1);
-		else if (map[cursor] == '\n')
-			line++;
-		else if (map[cursor] != "1" && line ==0)
-			return (0);
-		else if (map[cursor] == "P")
-			player++;
-		else if (map[cursor] == "E")
-			exit++;
-		else if (map[cursor] == "C")
-			collectible++;
-		cursor++;
-	}
+
 	if (player != 1 || exit != 1 || collectible == 0)
 		return (write(1, "Error, Map is invalide",22),1);
 }
