6898822bb91f4a1c9c24c93c0c8dcc51f37e7f40
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-12-03 16:51:17 +0100

    AutoSave 2024-12-03 16:51:17

--- so_long/map.c
+++ so_long/map.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:44 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/03 16:22:34 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:51:12 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_parsing(t_map map)
 {
 	int	i;
 	int	j;
+	int	len;
 
 	j= 0;
 	i = 0;
@@ -23,15 +24,18 @@ int	ft_parsing(t_map map)
 		{
 			while (map.map[i][j])
 			{
-				if (map.map[0][j] != '0')
-					return (write(1, "Error from map\n", 16));
-				
+				len = ft_strlen(map.map[i]);
+				if (map.map[0][j] != '1' || map.map[i][0] != '1'
+				 || map.map[i][len - 1] != '1' || map.map[i][0] != '1')
+					return (write(1, "Error from map\n", 16),0);
+				j++;
 			}
+			i++;
 		}
 	return (1);
 }
 
 int	ft_checkmap_line(t_map map)
 {
-
+	
 }
