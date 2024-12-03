013a3a748c149034144241d88733187c6b566235
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-12-03 16:21:07 +0100

    AutoSave 2024-12-03 16:21:07

--- so_long/map.c
+++ so_long/map.c
@@ -6,42 +6,30 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:44 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/03 15:03:57 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:20:52 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_parsing(char **map, int *player, int *exit, int *collectible)
-{
-	int	cursor;
+#include "so_long.h"
 
-	cursor = 0;
-		while (*map[cursor])
-	{
-		if (*map[0] != '0')
-			return (0);
-		else if (*map[cursor] == "P")
-			*player += 1;
-		else if (*map[cursor] == "E")
-			*exit += 1;
-		else if (*map[cursor] == "C")
-			*collectible += 1;
-		cursor++;
-	}
+int	ft_parsing(t_map map)
+{
+	int	i;
+	int	j;
+
+	j= 0;
+	i = 0;
+		while (map.map[i])
+		{
+			while (map.map[i][j])
+			{
+
+			}
+		}
 	return (1);
 }
 
-int	ft_checkmap_line(char *map)
+int	ft_checkmap_line(t_map map)
 {
-	int					cursor;
-	static int			player = 0;
-	static int			line = 0;
-	static int			exit = 0;
-	int					collectible;
 
-	collectible = 0;
-	cursor = 0;
-	if (ft_parsing(map, &line, &player, &exit, &collectible) == 0)
-		return (write(1, "Error, Map is invalide",22),1);
-	if (player != 1 || exit != 1 || collectible == 0)
-		return (write(1, "Error, Map is invalide",22),1);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,11 +6,18 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:52 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/03 15:04:11 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:16:26 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+typedef struct s_map
+{
+	char	**map;
+	int		player;
+	int		exit;
+	int		collectible;
+}	t_map;
 #endif
