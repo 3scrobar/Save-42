c1250c1d643ed9781073f71d07c6354a13a256b4
Thomas Le Sauter <tle-saut@c2r4p6.42perpignan.fr>
2024-12-03 14:50:39 +0100

    AutoSave 2024-12-03 14:50:39

--- so_long/map.c
+++ so_long/map.c
@@ -6,29 +6,32 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:44 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/03 14:44:48 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/03 14:50:39 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_checkmap(char *map)
 {
-	int			cursor;
-	static int	line;
-	int			player;
-	int			exit;
-	int			collectible;
+	int					cursor;
+	static int			player = 0;
+	static int			line = 0;
+	static int			exit = 0;
+	int					collectible;
 
-	player = 0;
-	exit = 0;
 	collectible = 0;
-	line = 0;
 	cursor = 0;
 	while (map[cursor])
 	{
 		if (map[cursor] != "1" && line ==0)
 			return (0);
-		else if (map[cursor])
+		else if (map[cursor] == "P")
+			player++;
+		else if (map[cursor] == "E")
+			exit++;
+		else if (map[cursor] == "C")
+			collectible++;
 		cursor++;
 	}
-	line++;
+	if (player != 1 || exit != 1 || collectible == 0)
+		return (0);
 }
