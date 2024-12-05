559b8e0ce509b76a4498adede6c59220918a24e3
Thomas Le Sauter <tle-saut@c3r1p2.42perpignan.fr>
2024-12-05 15:28:00 +0100

    AutoSave 2024-12-05 15:28:00

--- ftlib/ft_lstmap.c
+++ ftlib/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:01:34 by tle_saut          #+#    #+#             */
-/*   Updated: 2024/12/05 15:21:25 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/05 15:23:52 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new_node = ft_lstnew(f(current->content));
 		if (new_node == NULL)
 		{
+			del(new_node);
 			ft_lstclear(&result, del);
 			return (NULL);
 		}
