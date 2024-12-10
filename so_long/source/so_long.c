b5a9d642e9ac4d354395bdc96479281a8f160e57
Thomas Le Sauter <tle-saut@c1r2p3.42perpignan.fr>
2024-12-10 16:03:24 +0100

    AutoSave 2024-12-10 16:03:24

--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/10 15:56:04 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/10 16:03:21 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	main(int ac, char **av)
 								&img.endian);
 	my_mlx_pixel_put(&img, 8, 8, 0x00FF0000);
 	mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
+	mlx_hook(mlx.win, key)
 	mlx_loop(mlx.mlx);
 
 		
