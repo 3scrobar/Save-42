a9a32824cbed975bc5748b1c24b1bbf9b240d921
Thomas Le Sauter <tle-saut@c1r2p3.42perpignan.fr>
2024-12-10 14:37:55 +0100

    AutoSave 2024-12-10 14:37:55

--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/10 14:32:52 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/10 14:35:18 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ int	main(int ac, char **av)
 		return (1);
 	else
 		ft_printf("Map Valide, Launch The Game .....\n");
-	window = mlx
-	
+	window = mlx_new_window(mlx,800,600,"so_long");
+	if (window == NULL)
+		return 1;
+	mlx_loop(mlx);
 
 
 		
