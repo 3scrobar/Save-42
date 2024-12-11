20182794022306bd7fb2d36556e93fb185b01bb1
Thomas Le Sauter <tle-saut@c1r2p7.42perpignan.fr>
2024-12-11 15:03:15 +0100

    AutoSave 2024-12-11 15:03:15

--- so_long/source/map_utils2.c
+++ so_long/source/map_utils2.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 14:19:51 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/11 14:41:53 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/11 15:02:59 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,28 @@
 // Copie un tableau 2D
 char **copy_map(char **src, int rows)
 {
-	char **copy = malloc(sizeof(char *) * (rows + 1)); // +1 pour NULL
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	char **copy = malloc(sizeof(char *) * (rows + 1));
 	if (!copy)
 		return NULL;
 
-	for (int i = 0; i < rows; i++) {
-		copy[i] = strdup(src[i]); // Copie chaque ligne
-		if (!copy[i]) {
-			// Libérer en cas d'erreur
-			for (int j = 0; j < i; j++)
+	while(i < rows)
+	{
+		copy[i] = strdup(src[i]);
+		if (!copy[i])
+		{
+			while(j++ < i)
 				free(copy[j]);
 			free(copy);
 			return NULL;
 		}
+		i++;
 	}
-	copy[rows] = NULL; // Termine par NULL
+	copy[rows] = NULL;
 	return copy;
 }
 
@@ -40,25 +47,25 @@ t_map *copy_t_map(t_map *src)
 	if (!copy)
 		return NULL;
 
-	*copy = *src; // Copie les champs simples par valeur
+	*copy = *src;
 
-	// Copie le tableau 2D
 	if (src->map)
 		copy->map = copy_map(src->map, src->line);
 	else
 		copy->map = NULL;
-
 	return copy;
 }
 
 void free_t_map(t_map *map)
 {
+	int	i;
+	
+	i = 0;
 	if (!map)
 		return;
-
-	// Libérer le tableau 2D
-	if (map->map) {
-		for (int i = 0; map->map[i]; i++)
+	if (map->map)
+	{
+		whil(int i = 0; map->map[i]; i++)
 			free(map->map[i]);
 		free(map->map);
 	}
