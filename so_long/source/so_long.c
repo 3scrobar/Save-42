8d8ab1504ccd67b0f47a0ccb7bf8693d30906eee
Thomas Le Sauter <tle-saut@c2r4p8.42perpignan.fr>
2024-12-19 17:59:20 +0100

    AutoSave 2024-12-19 17:59:20

--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/19 17:57:00 by tle-saut         ###   ########.fr       */
+/*   Updated: 2024/12/19 17:59:09 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,15 +72,16 @@ int	check_coll(t_all *game, char *str)
 int key_press(int keycode, t_all *data)
 {
 	ft_putnbr_fd(keycode, 2);
+	printf("\n");
 	if (keycode == 65307)
 		close_window(data);
-	else if (keycode == 13  && check_coll(data, "haut") != 1) // W
+	else if (keycode == 119  && check_coll(data, "haut") != 1) // W
 		data->ybegin -= 1;
-	else if (keycode == 1  && check_coll(data, "bas") != 1) // S
+	else if (keycode == 115  && check_coll(data, "bas") != 1) // S
 		data->ybegin += 1;
-	else if (keycode == 0  && check_coll(data, "left") != 1) // A
+	else if (keycode == 97  && check_coll(data, "left") != 1) // A
 		data->xbegin -= 1;
-	else if (keycode == 2 && check_coll(data, "right") != 1) // D
+	else if (keycode == 100 && check_coll(data, "right") != 1) // D
 		data->xbegin += 1;
 	return (0);
 }
