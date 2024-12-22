1afa7284c1f8d89603ee9547039579b8495942af
VM_Maison <corsica-man@hotmail.fr>
2024-12-22 16:29:43 +0100

    AutoSave 2024-12-22 16:29:43

--- retry_so_long/source/so_long.c
+++ retry_so_long/source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: groot <groot@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/22 16:24:23 by groot            ###   ########.fr       */
+/*   Updated: 2024/12/22 16:29:16 by groot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,38 @@ int	main(int ac, char **av)
 	if (ft_total_check(ac, &game) || game.mlx == NULL)
 		return (1);
 	return (0);
+}
+char	**ft_init_tab(int fd)
+{
+	int		a;
+	char	buffer[BUFFER_SIZE + 1];
+	char	**tab;
+	char	*line;
+
+	line = NULL;
+	while (1)
+	{
+		a = read(fd, buffer, BUFFER_SIZE);
+		if (a < 1)
+			break ;
+		if (line == NULL)
+			line = ft_strdup(buffer);
+		else
+			line = ft_strjoinfree(&line, buffer);
+	}
+	tab = ft_split(line, '\n');
+	free(line);
+	return (tab);
+}
+int	ft_init_map(t_all *map, char *path)
+{
+	int		fd;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (ft_putstr_fd("Error from file reading\n", 2), 1);
+	map->map = ft_init_tab(fd);
+
 }
 int ft_total_check(int ac, t_all *game)
 {
