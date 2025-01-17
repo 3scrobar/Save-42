ecba37104e24ca8fc0599908b5ea064a1dfc0df2
42 Schol <corsica-man@hotmail.fr>
2025-01-17 17:28:48 +0100

    norm 2

--- Mac so_long/source/colision.c
+++ Mac so_long/source/colision.c
@@ -37,37 +37,40 @@ int	ft_collision_check(t_all *all)
 
 int	ft_colision_right(t_all *all)
 {
-	int right;
+	int	right;
 
-	right  = (all->xstart + 58) / all->tile_size;
+	right = (all->xstart + 58) / all->tile_size;
 	if (all->map[((all->ystart + 32) / all->tile_size)][right] == '1')
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
+
 int	ft_colision_left(t_all *all)
 {
-	int left;
+	int	left;
 
-	left   = ((all->xstart + 10)/ all->tile_size);
+	left = ((all->xstart + 10) / all->tile_size);
 	if (all->map[(all->ystart + 48) / all->tile_size][left] == '1')
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
+
 int	ft_colision_up(t_all *all)
 {
-	int top;
+	int	top;
 
-	top    = ((all->ystart + 31)/ all->tile_size);
+	top = ((all->ystart + 31) / all->tile_size);
 	if (all->map[top][(all->xstart + 32) / all->tile_size] == '1' || top == 0)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
+
 int	ft_colision_down(t_all *all)
 {
-	int bottom;
+	int	bottom;
 
-	bottom = ((all->ystart + 65) / 64 );
+	bottom = ((all->ystart + 65) / 64);
 	if (all->map[bottom][(all->xstart + 32) / all->tile_size] == '1')
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
