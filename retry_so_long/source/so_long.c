76953df98bca4bd2ac33ab68b1e1a5c25e1f3d95
42 Schol <corsica-man@hotmail.fr>
2025-01-20 14:17:36 +0100

    dgg

--- retry_so_long/source/so_long.c
+++ retry_so_long/source/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: groot <groot@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 14:34:49 by tle-saut          #+#    #+#             */
-/*   Updated: 2024/12/22 17:36:25 by groot            ###   ########.fr       */
+/*   Updated: 2025/01/20 14:10:56 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,205 +20,60 @@ int	main(int ac, char **av)
 	game.mlx = mlx_init();
 	if (ft_total_check(ac, &game, av, &cpy) != 0 || game.mlx == NULL)
 		return (1);
-	if (ft_flood_path(&cpy, cpy.ystart, cpy.xstart) != 0 
-		|| ft_check_after_flood(&cpy) != 0)
-		return (ft_putstr_fd("Error from Path\n", 2), 1);
+	ft_printf("Check for the flood\n");
+	if (ft_flood_path(&cpy, cpy.ystart, cpy.xstart) != 0
+		|| ft_check_after_flood(&cpy) != 0 || ft_init_img(&game) != 0)
+		return (ft_putstr_fd("Error\n", 2), 1);
+	game.win = mlx_new_window(game.mlx, game.column * 64,
+			(game.line + 1) * 64, "so_long");
+	game.xstart = game.xstart * game.tile_size;
+	game.ystart = game.ystart * game.tile_size;
+	ft_init_var(&game);
+	mlx_hook(game.win, 17, 1L << 17, ft_close, &game);
+	mlx_hook(game.win, 2, 1L << 0, ft_handle_key_press, &game);
+	mlx_hook(game.win, 3, 1L << 1, ft_handle_key_release, &game);
+	mlx_loop_hook(game.mlx, ft_game_loop, &game);
+	mlx_loop(game.mlx);
 	return (0);
 }
-char	**ft_init_tab(int fd)
+
+int	ft_game_loop(t_all *game)
 {
-	int		a;
-	char	buffer[BUFFER_SIZE + 1];
-	char	**tab;
-	char	*line;
-
-	line = NULL;
-	while (1)
-	{
-		a = read(fd, buffer, BUFFER_SIZE);
-		if (a < 1)
-			break ;
-		if (line == NULL)
-			line = ft_strdup(buffer);
-		else
-			line = ft_strjoinfree(&line, buffer);
-	}
-	tab = ft_split(line, '\n');
-	free(line);
-	return (tab);
-}
-int	ft_init_map(t_all *map, char *path)
-{
-	int		fd;
-
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return (ft_putstr_fd("Error from file reading\n", 2), 1);
-	map->map = ft_init_tab(fd);
-	map->player = 0;
-	map->exit = 0;
-	map->collectible = 0;
-	map->column = 0;
-	map->line = 0;
-
-	close(fd);
+	mlx_clear_window(game->mlx, game->win);
+	draw_map(game);
+	ft_if_gameloop(game);
+	ft_velocity_apply(game);
+	ft_collision_check(game);
+	ft_security_check(game);
+	mlx_string_put(game->mlx, game->win, 880, 20, 0xFF0000, "Nbr PDV : ");
+	mlx_string_put(game->mlx, game->win, 980, 20, 0xFF0000, ft_itoa(game->hp));
+	ft_enemy(game);
 	return (0);
 }
-int ft_total_check(int ac, t_all *game, char **av, t_all *cpy)
-{
-	(void)game;
-	if (ac != 2)
-		return (ft_putstr_fd("Wrong Arguments\n", 2), 1);
-	if (ft_init_map(game, av[1]) != 0)
-		return (ft_putstr_fd("Problem from Map init\n", 2), 1);
-	if (ft_give_all_nbpoint(game) != 0 || ft_check_square(game) != 0)
-		return (ft_putstr_fd("Error from map design\n", 2), 1);
-	if (game->player != 1)
-		return (ft_putstr_fd("Problem from Start POS\n", 2), 1);
-	if (game->exit != 1)
-		return (ft_putstr_fd("Problem from Exit POS\n", 2), 1);
-	if (game->collectible == 0)
-		return (ft_putstr_fd("No Collectible\n", 2), 1);
-	ft_give_start_POS(game);
-	ft_copy_struct(game, cpy);
-	if(ft_check_border(cpy) != 0)
-		return (1);
-	if(ft_flood_path(cpy, cpy->ystart, cpy->xstart) != 0 
-		|| ft_check_after_flood(cpy) != 0)
-		return (ft_putstr_fd("Error from Path\n", 2), 1);
-	return (ft_printf("Everything is Good, Launch the Game ...\n",0));
-}
-void	ft_copy_struct(t_all *game, t_all *cpy)
-{
-	cpy->mlx = game->mlx;
-	cpy->win = game->win;
-	cpy->map = game->map;
-	cpy->line = game->line;
-	cpy->column = game->column;
-	cpy->player = game->player;
-	cpy->exit = game->exit;
-	cpy->collectible = game->collectible;
-	cpy->xstart = game->xstart;
-	cpy->ystart = game->ystart;
-}
-int	ft_tablen(char	**tab)
-{
-	int	i;
 
-	i = 0;
-	while (tab[i])
-		i++;
-	return (i - 1);
-}
-int	ft_give_all_nbpoint(t_all *map)
+void	ft_if_gameloop(t_all *game)
 {
-	int	i;
-	int	j;
+	if (game->xvelocity < 0)
+		mlx_put_image_to_window(game->mlx, game->win, game->imgplayerleft,
+			game->xstart, game->ystart);
+	if (game->xvelocity > 0)
+		mlx_put_image_to_window(game->mlx, game->win, game->imgplayerright,
+			game->xstart, game->ystart);
+	if (game->xvelocity == 0)
+		mlx_put_image_to_window(game->mlx, game->win, game->imgplayercenter,
+			game->xstart, game->ystart);
+	if (game->enemyvelocity < 0 && game->destroyenemy == 0)
+		mlx_put_image_to_window(game->mlx, game->win, game->imgenemyleft,
+			game->xenemy, game->yenemy);
+	if (game->enemyvelocity > 0 && game->destroyenemy == 0)
+		mlx_put_image_to_window(game->mlx, game->win, game->imgenemyright,
+			game->xenemy, game->yenemy);
+	if (game->hp == 0)
+		ft_close(game);
+}
 
-	i = 0;
-	map->line = ft_tablen(map->map);
-	map->column = ft_strlen(map->map[i]);
-	while (map->map[i])
-	{
-		if (ft_strlen(map->map[i]) != map->column)
-			return (ft_putstr_fd("Line is not the same size\n", 2), 1);
-		j = 0;
-		while (map->map[i][j])
-		{
-			if (map->map[i][j] == 'P')
-				map->player += 1;
-			else if (map->map[i][j] == 'E')
-				map->exit += 1;
-			else if (map->map[i][j] == 'C')
-				map->collectible += 1;
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-int	ft_check_square(t_all *map)
+int	ft_close(t_all *game)
 {
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (map->map[i])
-	{
-		j = 0;
-		while (map->map[i][j])
-			j++;
-		if (j != map->column)
-			return (ft_putstr_fd("Line as not the same size\n", 2), 1);
-		i++;
-	}
-	return (0);
-}
-int	ft_give_start_POS(t_all *game)
-{
-	game->ystart = 0;
-	while (game->map[game->ystart])
-	{
-		game->xstart = 0;
-		while (game->map[game->ystart][game->xstart])
-		{
-			if (game->map[game->ystart][game->xstart] == 'P')
-				return (0);
-			game->xstart++;
-		}
-		game->ystart++;
-	}
-	return (ft_putstr_fd("Starting POS Missing\n", 2), 1);
-}
-int	ft_flood_path(t_all *game, size_t ystart, size_t xstart)
-{
-	if (game->map[ystart][xstart] == '1')
-		return (0);
-	if (game->map[ystart][xstart] == '0' || game->map[ystart][xstart] == 'P' ||
-		game->map[ystart][xstart] == 'E' || game->map[ystart][xstart] == 'C')
-		game->map[ystart][xstart] = '1';
-	ft_flood_path(game, ystart + 1, xstart);
-	ft_flood_path(game, ystart - 1, xstart);
-	ft_flood_path(game, ystart, xstart + 1);
-	ft_flood_path(game, ystart, xstart - 1);
-	return (0);
-}
-int	ft_check_after_flood(t_all *game)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (game->map[i])
-	{
-		j = 0;
-		while (game->map[i][j])
-		{
-			if (game->map[i][j] != '1')
-				return (ft_putstr_fd("Path is invalid\n", 2), 1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-int	ft_check_border(t_all *map)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < map->column)
-	{
-		if (map->map[0][i] != '1' || map->map[map->line][i] != '1')
-			return (ft_putstr_fd("Error from map border\n", 2), 1);
-		i++;
-	}
-	i = 0;
-	while (i <= map->line)
-	{
-		if (map->map[i][0] != '1' || map->map[i][map->column - 1] != '1')
-			return (ft_putstr_fd("Error from map border\n", 2), 1);
-		i++;
-	}
-	return (0);
+	mlx_destroy_window(game->mlx, game->win);
+	exit(0);
 }
