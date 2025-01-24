9c527d6e7c58390cb140de31544b489aa618482b
MacBook Pro <corsica-man@hotmail.fr>
2025-01-24 17:13:29 +0100

    AutoSave 2025-01-24 17:13:29

--- push_swap/source/main.c
+++ push_swap/source/main.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:54:07 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/01/24 15:04:55 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/01/24 17:12:48 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	main(int ac, char **av)
 {
+	t_list lsta;
+	t_list lstb;
+
+	(void)lstb;
 	if (ac != 2)
-		ft_putstr_fd("Error from argument",2);
-	
-	
-	
+		ft_putstr_fd("Error from argument\n",2);
+	check_args(av[1]);
+	give_nb(av[1], &lsta);
+	ft_lstiter(&lsta, print_content);
 	return (0);
 }
 int	check_args(char *str)
@@ -31,8 +35,33 @@ int	check_args(char *str)
 		if ((str[i] >= '0' && str[i] < '9') || str[i] == ' ' || str[i] == '-' )
 			i++;
 		else
-			return(ft_putstr_fd("Error put some good args", 2));
+		{
+			return (ft_putstr_fd("Error put some good args\n", 2), 1);
+		}
 	}
-
 	return (0);
+}
+int	give_nb(char *str, t_list *lst)
+{
+	t_list *tmp;
+	int i;
+
+	while (*str)
+		{
+			if (*str == ' ')
+				str++;
+			else
+			{
+				i = ft_atoi(str);
+				tmp = ft_lstnew(&i);
+				ft_lstadd_back(&lst, tmp);
+				while (*str && *str != ' ')
+					str++;
+			}
+		}
+	return (0);
+}
+void print_content(void *content)
+{
+    printf("%d\n", *(int *)content);
 }
--- push_swap/source/push_swap.h
+++ push_swap/source/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:55:42 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/01/24 14:57:29 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/01/24 17:11:49 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define PUSH_SWAP_H
 
 # include "../libft/source/libft.h"
+# include <stdio.h>
+
+int	check_args(char *str);
+int	give_nb(char *str, t_list *lst);
+void print_content(void *content);
 
 
 #endif
