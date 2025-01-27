21eeccc184e6ff70d9e097a87ff68f4ad44d81d9
42 Schol <corsica-man@hotmail.fr>
2025-01-27 19:23:39 +0100

    AutoSave 2025-01-27 19:23:39

--- push_swap/source/main.c
+++ push_swap/source/main.c
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:54:07 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/01/24 22:26:14 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/01/27 19:19:40 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,201 @@
 
 int	main(int ac, char **av)
 {
-	t_list lsta;
-	t_list lstb;
+	p_swap *lista;
+	p_swap *listb;
 
-	lsta.content = NULL;
-	lsta.next = NULL;
-	lstb.content = NULL;
-	lstb.next = NULL;
+	lista = NULL;
+	listb = NULL;
 	if (ac != 2)
-		ft_putstr_fd("Error from argument\n",2);
-	check_args(av[1]);
-	give_nb(av[1], &lsta);
-	ft_lstiter(&lsta, print_content);
+		return (ft_putstr_fd("Error from argument\n",2), 1);
+	check_args(av[1], lista);
+	ra(&lista);
+	lstiter(lista, ft_printfnumb);
+	
 	return (0);
 }
-int	check_args(char *str)
+int	check_args(char *str, p_swap *lst)
+{
+	long int	i;
+
+	i = 0;
+	while (*str)
+	{
+		if ((*str < '0' || *str > '9') && (*str != '-'))
+			return (ft_putstr_fd("Error from NUMBER\n", 2), 1);
+		if ((*str >= '0' && *str <= '9') || *str == '+' || *str == '-' )
+			{
+				i = atoi_long(str);
+				if (i > INT_MAX || i < INT_MIN)
+					return (ft_putstr_fd("Error int\n",2),1);
+				if(lstcomp(lst, i) != 0)
+					return (ft_putstr_fd("Error dup\n", 2), 1);
+				lstadd_back(&lst, lstnew((int)i));
+			}
+			
+		 while (*str && *str != ' ')
+				str++;
+		if (*str != '\0')
+			str++;
+	}
+	lstiter(lst, ft_printfnumb);
+	
+	return (0);
+}
+
+void	ra(p_swap **lst)
+{
+	p_swap *temp;
+	
+	ft_printf("%d\n", (*lst)->value);
+	temp = *lst;
+	ft_printf("%d", temp->value);
+	temp->chain = NULL;
+	lstadd_back(lst, temp);
+	*lst = (*lst)->chain;
+}
+
+void	lstadd_back(p_swap **lst, p_swap *new)
+{
+	p_swap	*new_one;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	new_one = *lst;
+	while (new_one->chain != NULL)
+		new_one = new_one->chain;
+	new_one->chain = new;
+}
+
+void	lstadd_front(p_swap **lst, p_swap *new)
+{
+	new->chain = *lst;
+	*lst = new;
+}
+
+void	lstclear(p_swap **lst, void (*del)(int))
+{
+	p_swap	*temp;
+
+	while (*lst != NULL)
+	{
+		temp = (*lst)->chain;
+		lstdelone(*lst, del);
+		*lst = temp;
+	}
+}
+
+void	lstdelone(p_swap *lst, void (*del)(int))
+{
+	if (lst == NULL)
+		return ;
+	del(lst -> value);
+	free(lst);
+}
+
+void	lstiter(p_swap *lst, int (*f)(int))
+{
+	while (lst != NULL)
+	{
+		f(lst -> value);
+		lst = lst->chain;
+	}
+}
+int	lstcomp(p_swap *lst, int nbr)
+{
+	while (lst != NULL)
+	{
+		if(nbr == lst->value)
+			return(1);
+		lst = lst->chain;
+	}
+	return (0);
+}
+
+p_swap	*lstlast(p_swap *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->chain != NULL)
+	{
+		lst = lst->chain;
+	}
+	return (lst);
+}
+
+p_swap	*lstmap(p_swap *lst, int (*f)(int), void (*del)(int), void (*delv)(void *))
+{
+	p_swap	*current;
+	p_swap	*result;
+	p_swap	*new_node;
+
+	result = NULL;
+	current = lst;
+	if (!lst || !f || !del)
+		return (NULL);
+	while (current != NULL)
+	{
+		new_node = lstnew(f(current->value));
+		if (new_node == NULL)
+		{
+			delv(new_node);
+			lstclear(&result, del);
+			return (NULL);
+		}
+		lstadd_back(&result, new_node);
+		current = current->chain;
+	}
+	return (result);
+}
+
+p_swap	*lstnew(int value)
+{
+	p_swap	*new_node;
+
+	new_node = malloc(sizeof(p_swap));
+	if (new_node == NULL)
+		return (NULL);
+	new_node -> chain = NULL;
+	new_node -> value = value;
+	return (new_node);
+}
+
+int	lstsize(p_swap *lst)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (lst != NULL)
 	{
-		if ((str[i] >= '0' && str[i] < '9') || str[i] == ' ' || str[i] == '-' )
-			i++;
-		else
-		{
-			return (ft_putstr_fd("Error put some good args\n", 2), 1);
-		}
+		lst = lst -> chain;
+		i++;
 	}
-	return (0);
+	return (i);
 }
-int	give_nb(char *str, t_list *lst)
+long int	atoi_long(const char *nptr)
 {
-	int i;
+	int			i;
+	int			neg;
+	long int	c;
 
-	while (*str)
-		{
-			if (*str == ' ')
-				str++;
-			else
-			{
-				i = ft_atoi(str);
-				ft_lstadd_back(&lst, ft_lstnew(&i));
-			}
-			str++;
-		}
-	return (0);
-}
-void print_content(void *content)
-{
-    printf("%d\n", *(int *)content);
+	i = 0;
+	c = 0;
+	neg = 1;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			neg = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		c = (c * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (c * neg);
 }
--- push_swap/source/push_swap.h
+++ push_swap/source/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:55:42 by tle-saut          #+#    #+#             */
-/*   Updated: 2025/01/24 17:11:49 by tle-saut         ###   ########.fr       */
+/*   Updated: 2025/01/27 18:40:28 by tle-saut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,28 @@
 # define PUSH_SWAP_H
 
 # include "../libft/source/libft.h"
-# include <stdio.h>
+# include <limits.h>
+/*******************************STRUCTURE*************************************/
 
-int	check_args(char *str);
-int	give_nb(char *str, t_list *lst);
-void print_content(void *content);
+typedef struct p_ush
+{
+	int				value;
+	struct p_ush	*chain;
+}					p_swap;
 
+/*******************************FUNCTION**************************************/
+
+int	lstsize(p_swap *lst);
+p_swap	*lstnew(int value);
+p_swap	*lstmap(p_swap *lst, int (*f)(int), void (*del)(int), void (*delv)(void *));
+void	lstiter(p_swap *lst, int (*f)(int));
+void	lstdelone(p_swap *lst, void (*del)(int));
+void	lstclear(p_swap **lst, void (*del)(int));
+void	lstadd_front(p_swap **lst, p_swap *new);
+void	lstadd_back(p_swap **lst, p_swap *new);
+int		check_args(char *str, p_swap *lst);
+long int	atoi_long(const char *nptr);
+int	lstcomp(p_swap *lst, int nbr);
+void	ra(p_swap **lst);
 
 #endif
