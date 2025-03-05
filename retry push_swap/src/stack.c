79c38e741f74fb0ba6a05ff99299bea507aacfad
MacBook Pro <corsica-man@hotmail.fr>
2025-03-05 13:11:09 +0100

    AutoSave 2025-03-05 13:11:09

---
+++ retry push_swap/src/stack.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 12:38:59 by tle-saut          #+#    #+#             */
+/*   Updated: 2025/03/05 12:56:32 by tle-saut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+/* get_stack_bottom:
+*	Returns the last element of the stack.
+*/
+t_stack	*get_stack_bottom(t_stack *stack)
+{
+	while (stack && stack->next != NULL)
+		stack = stack->next;
+	return (stack);
+}
+
+/* Returns the second to last element of the stack. */
+t_stack	*get_stack_before_bottom(t_stack *stack)
+{
+	while (stack && stack->next && stack->next->next != NULL)
+		stack = stack->next;
+	return (stack);
+}
+
+t_stack	*stack_new(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof * new);
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = 0;
+	new->pos = -1;
+	new->target_pos = -1;
+	new->cost_a = -1;
+	new->cost_b = -1;
+	new->next = NULL;
+	return (new);
+}
+
+void	stack_add_bottom(t_stack **stack, t_stack *new)
+{
+	t_stack	*tail;
+
+	if (!new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	tail = get_stack_bottom(*stack);
+	tail->next = new;
+}
+
+int	get_stack_size(t_stack	*stack)
+{
+	int	size;
+
+	size = 0;
+	if (!stack)
+		return (0);
+	while (stack)
+	{
+		stack = stack->next;
+		size++;
+	}
+	return (size);
+}
---
+++ retry push_swap/src/swap.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-saut <tle-saut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 12:39:02 by tle-saut          #+#    #+#             */
+/*   Updated: 2025/03/05 12:56:49 by tle-saut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+static void	swap(t_stack *stack)
+{
+	int	tmp;
+
+	if (stack == NULL || stack->next == NULL)
+		return ;
+	tmp = stack->value;
+	stack->value = stack->next->value;
+	stack->next->value = tmp;
+	tmp = stack->index;
+	stack->index = stack->next->index;
+	stack->next->index = tmp;
+}
+
+void	do_sa(t_stack **stack_a)
+{
+	swap(*stack_a);
+	ft_putstr("sa\n");
+}
+
+void	do_sb(t_stack **stack_b)
+{
+	swap(*stack_b);
+	ft_putstr("sb\n");
+}
+
+void	do_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	swap(*stack_a);
+	swap(*stack_b);
+	ft_putstr("ss\n");
+}
